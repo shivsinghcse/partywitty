9de56dc139a19d76a37ee916a67c6635ee06f139
shivsinghcse <shivsinghcse@gmail.com>
2026-05-15 15:09:11 +0530

    completed

--- src/components/VerificationIntro.jsx
+++ src/components/VerificationIntro.jsx
@@ -1,34 +1,49 @@
 
-import { X, CheckCircle } from 'lucide-react'
+import { CircleX, CheckCircle, Smile, BadgeCheck } from 'lucide-react'
 
 const VerificationIntro = ({ onStart, onBack }) => {
   return (
-    <div className="min-h-screen flex items-center justify-center p-6 bg-gray-100">
-      <div className="relative bg-white rounded- w-full max-w- p-8">
-        <button onClick={onBack} className="absolute top-4 right-4">
-          <X className="w-5 h-5 text-gray-400" />
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="relative bg-white/70 backdrop-blur-[100px] rounded-lg w-200 py-8 px-8 shadow-2xl border border-gray-200 flex flex-col items-center gap-5">
+        <button
+              onClick={onBack}
+              className="absolute top-2 right-2 z-20 w-8 h-8 bg-[#E8E6F5] rounded-full flex items-center justify-center hover:cursor-pointer"
+        >
+              
+          <CircleX className="w-6 h-6 text-[#7464E4]"/>
         </button>
         
-        <div className="flex justify-center mb-6">
-          <div className="w-20 h-20 bg-gray-100 rounded-full flex items-center justify-center">
-            <div className="w-12 h-12 border-2 border-gray-300 rounded-full flex items-center justify-center">
-              <div className="w-6 h-6 bg-gray-300 rounded-full" />
+        <div className="flex flex-col gap-5 py-5 justify-center items-center  w-127.5 ">
+
+          <div className='flex flex-col gap-5'>
+            <div className="relative w-70 h-52.5 border-2 border-dashed border-[#4F4F4F4D] rounded-[48px] flex items-center justify-center">
+              <div className="w-38.75 h-51.5 border-4 border-[#4F4F4F] rounded-full flex items-center justify-center">
+                <Smile size={60} color='#4F4F4F' />
+              </div>
+              <button className='absolute -top-2 -right-2 w-9.5 h-9.5 flex items-center justify-center rounded-full bg-[#2B183466]'>
+                <BadgeCheck size={18} color='#4F4F4F'/>
+              </button>
             </div>
+
           </div>
+
+          <div className='px-2 flex flex-col gap-5'>
+            <h2 className="font-[Lexend] text-2xl font-semibold leading-[100%] tracking-[-1.2px] text-center align-middle text-[#4F4F4F]">You're one step away</h2>
+            <p className="text-lg font-medium leading-[100%] tracking-normal text-[#4F4F4F] font-[Lexend] text-center">
+              Verify your profile to send this invite and connect with people around you.
+            </p>
+          </div>  
         </div>
 
-        <h2 className="font-[Lexend] text-2xl font-bold text-center mb-2">You're one step away</h2>
-        <p className="text-sm text-gray-600 font-[Lexend] text-center mb-6">
-          Verify your profile to send this invite and connect with people around you.
-        </p>
+        
 
-        <div className="space-y-3 mb-6">
+        <div className="flex px-2 pb-2 gap-4 justify-between w-full">
           {[
             'Builds trust instantly',
             'Better chances she accepts',
             'Unlock special invites'
           ].map(text => (
-            <div key={text} className="flex items-center gap-2 text-sm font-[Lexend]">
+            <div key={text} className="flex items-center gap-2 text-base font-medium tracking-normal leading-[100%] text-[#4F4F4F] font-[Lexend]">
               <CheckCircle className="w-5 h-5 text-green-500" />
               <span>{text}</span>
             </div>
@@ -37,11 +52,11 @@ const VerificationIntro = ({ onStart, onBack }) => {
 
         <button 
           onClick={onStart}
-          className="w-full bg-gradient-to-r from-[#C229C0] to-[#FC4B6A] text-white py-4 rounded-full font-[Lexend] font-semibold mb-3"
+          className="w-fit  bg-gradient-to-r from-[#C229C0] to-[#FC4B6A] text-white py-3.5 px-24 leading-[100%] tracking-normal text-center align-middle rounded-full font-[Lexend] font-normal"
         >
           Verify & Send Invite
         </button>
-        <p className="text-xs text-gray-400 text-center font-[Lexend]">TAKES LESS THAN 60 SECONDS</p>
+        <p className="text-[12.5px] font-medium tracking-normal leading-[16.5px] align-middle uppercase text-[#4F4F4F] text-center font-[Lexend]">TAKES LESS THAN 60 SECONDS</p>
       </div>
     </div>
   )
